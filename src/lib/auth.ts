9e888f7e85f116a4820e6af4052325b9cbe69823
Romain Bourdain <romain.bourdain93@gmail.com>
2024-07-12 20:20:37 +0200

    prisma adapter

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,11 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
 
+const prisma = new PrismaClient();
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [Github],
 });
